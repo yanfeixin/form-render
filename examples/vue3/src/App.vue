a6eb0102831ace9cf9b989c8bafce62b45985a14
caohao <caohao@raydeMacBook-Pro.local>
2023-12-05 19:54:58 +0800

    增加vscode工作区配置

--- examples/vue3/src/App.vue
+++ examples/vue3/src/App.vue
@@ -1,12 +1,16 @@
 <!--
  * @Author: caohao
- * @Date: 2023-12-05 09:53:34
+ * @Date: 2023-12-05 19:45:03
  * @LastEditors: caohao
- * @LastEditTime: 2023-12-05 19:33:49
- * @Description: b
+ * @LastEditTime: 2023-12-05 19:52:45
+ * @Description: 
 -->
 <template>
-  <div>
-    <k-button type="error"></k-button>
-  </div>
+  <HelloWorld></HelloWorld>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style></style>
