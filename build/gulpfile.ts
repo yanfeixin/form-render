b34c5d424f6c758a63f6152501fbbf709ba5bbf0
caohao <caohao@raydeMacBook-Pro.local>
2023-12-20 18:06:54 +0800

    增加环境变量文件

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -4,22 +4,32 @@
  * @Autor: caohao
  * @Date: 2023-10-04 01:03:22
  * @LastEditors: caohao
- * @LastEditTime: 2023-12-19 18:16:03
+ * @LastEditTime: 2023-12-20 11:21:15
  */
 import { deleteSync } from 'del'
 import type { TaskFunction } from 'gulp'
 import { series, parallel } from 'gulp'
+import minimist from 'minimist'
+import dotenv from 'dotenv'
 // import { title } from './utils/message'
 // import { buildOutput, buildRoot } from './utils/paths'
 import { buildCdnModules, buildNodeModules, buildTheme, generateTypesDefinitions, run, title, buildOutput, buildRoot } from './src'
 import { copyTypesDefinitions } from './src/tasks'
-// import { buildTheme } from './tasks/theme/gulpfile.prod'
-// import { run } from './utils/process'
-// import { generateTypesDefinitions } from './tasks/typeExport'
+import { resolve } from 'node:path'
+
 console.log(title('开始构建～～！'))
 export const clean: TaskFunction = (done) => {
   //   console.log(title("Starting!"))
   deleteSync(buildOutput, { force: true })
+
+  done()
+}
+export const loadEnv: TaskFunction = (done) => {
+  const argvs = minimist(process.argv.slice(2))
+  const { mode } = argvs
+  const envPath = resolve(buildRoot, `.env${mode ? `.${mode}` : ``}`)
+  console.log(envPath)
+  dotenv.config({ path: envPath })
   done()
 }
 export const withTaskName = <T extends TaskFunction>(name: string, fn: T) => Object.assign(fn, { displayName: name })
@@ -37,4 +47,4 @@ export const testModel = async () => {
   console.log(111)
 }
 //  parallel(copyTypesDefinitions)
-export default series(clean, buildTheme, parallel(buildCdnModules, buildNodeModules, generateTypesDefinitions), parallel(copyTypesDefinitions))
+export default series(clean, loadEnv, buildTheme, parallel(buildCdnModules, buildNodeModules, generateTypesDefinitions), parallel(copyTypesDefinitions))
